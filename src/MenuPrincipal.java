7ba111f483a6ff4194057a050f050e936d132559
Daiki Benedek Rueda Tokuhara <daikibenedek@ciencias.unam.mx>
2022-09-15 00:21:01 -0500

    Menu avanzado sin metodos terminados

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -45,18 +45,15 @@ public class MenuPrincipal {
       switch (opcion) {
         case 1:
           System.out.println("Has seleccionado la opcion 1 Gestion de Viveros");
-          ViverosMain ViverosMain = new ViverosMain();
-          ViverosMain.showMenuViveros();
+          menuViveros();
           break;
         case 2:
           System.out.println("Has seleccionado la opcion 2 Gestion de Empleados");
-          EmpleadosMain EmpleadosMain = new EmpleadosMain();
-          EmpleadosMain.showMenuEmpleados();
+          menuEmpleados();
           break;
         case 3:
-          System.out.println("Has seleccionado la opcion 3 Gestion de PLantas");
-          PlantasMain PlantasMain = new PlantasMain();
-          PLantasMain.showMenuPlantas();
+          System.out.println("Has seleccionado la opcion 3 Gestion de Plantas");
+          menuPlantas();
           break;
         case 5:
           salir = true;
@@ -68,8 +65,124 @@ public class MenuPrincipal {
     catch (IllegalArgumentException e) {
       System.out.println("Argumento invalido, favor de introducir solo números");
     }
-  }
 
-  }
+    public void menuViveros(){
+      // getInt esta definido hasta abajo y dice que hace
+      int a = getInt(5,"¿Qué deseas realizar? \n Escribe 0 para agregar un nuevo VÍVERO \n Escribe 1 para consultar un VÍVERO \n Escribe 2 para editar un VÍVERO \n Escribe 3 para eliminar un VÍVERO \n Escribe 4 para guardar cambios o regresar", "Error, eso no es una opción")
+      switch (a){
+        case (0):
+          //LOS METODOS DE LOS CSV RECIBEN ALGO EN ESPECIFICO, NO TODOS UN ENTERO
+          agregarCSV(1);
+        break;
+                
+        case (1):
+          consultarCSV(1);
+        break;
+                
+        case (2):
+          editarCSV(1);
+        break;
 
+        case (3):
+          eliminarCSV(1);
+        break;
+        
+        case (4):
+        break;
+
+        default:
+          System.out.println("Elige una opción válida");
+        break;
+      }
+    }
+
+    public void menuEmpleados(){
+      // getInt esta definido hasta abajo y dice que hace
+      int a = getInt(5,"¿Qué deseas realizar? \n Escribe 0 para agregar un nuevo EMPLEADO \n Escribe 1 para consultar un EMPLEADO \n Escribe 2 para editar un EMPLEADO \n Escribe 3 para eliminar un EMPLEADO \n Escribe 4 para guardar cambios o regresar", "Error, eso no es una opción")
+      switch (a){
+        case (0):
+          //LOS METODOS DE LOS CSV RECIBEN ALGO EN ESPECIFICO, NO TODOS UN ENTERO
+          agregarCSV(2);
+        break;
+                
+        case (1):
+          consultarCSV(2);
+        break;
+                
+        case (2):
+          editarCSV(2);
+        break;
+
+        case (3):
+          eliminarCSV(2);
+        break;
+
+        case (4):
+        break;
+                
+        default:
+          System.out.println("Elige una opción válida");
+        break;
+      }
+    }
+
+    public void menuPlantas(){
+      // getInt esta definido hasta abajo y dice que hace
+      int a = getInt(5,"¿Qué deseas realizar? \n Escribe 0 para agregar una nueva PLANTA \n Escribe 1 para consultar una PLANTA \n Escribe 2 para editar una PLANTA \n Escribe 3 para eliminar una PLANTA \n Escribe 4 para guardar cambios o regresar", "Error, eso no es una opción")
+      switch (a){
+        case (0):
+          //LOS METODOS DE LOS CSV RECIBEN ALGO EN ESPECIFICO, NO TODOS UN ENTERO
+          agregarCSV(3);
+        break;
+                
+        case (1):
+          consultarCSV(3);
+        break;
+                
+        case (2):
+          editarCSV(3);
+        break;
+
+        case (3):
+          eliminarCSV(3);
+        break;
+
+        case (4):
+        break;
+                
+        default:
+          System.out.println("Elige una opción válida");
+        break;
+      }
+    }
+  
+       
+
+
+    /**
+     *getInt recibe un entero, dos cadenas y devuelve un entero, si lo que el usuario ponga no es un entero, devolverá error
+     * hasta que se le pase un entero, es para verificar si es un entero o no. La primera cadena es el mensaje
+     * que queremos que el usuario vea y la segunda es el error. el entero que recibe es la cota superior.
+     *@param el numero entero
+     *@return el entero
+     */
+    public int getInt(int d, String b,String c){
+      Scanner sc = new Scanner(System.in);
+      int a=0;
+      boolean z=false;
+      do{
+      System.out.println(b);
+      if(sc.hasNextInt() && a<d){
+        z = true;
+        a = sc.nextInt();
+      }
+      else {
+        sc.next();
+        System.out.println(c);
+        
+      }
+      }while(z == false);
+        return a;
+    }
+  }
 }
